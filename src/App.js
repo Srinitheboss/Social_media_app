3d1eb5f9f5c7f53686d182fbb2bc241f4161fd87
Srinitheboss <srinivasanmk73@gmail.com>
2025-03-19 21:21:40 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
-
+import Header from "./Header.js";
+import Nav from "./Nav";
 function App() {
-  return (
-  );
+return (
+<div className = "App">
+<Header title = "Chat-Chat"/>
+<Nav />
+</div>
+);
 }
 
 export default App;
